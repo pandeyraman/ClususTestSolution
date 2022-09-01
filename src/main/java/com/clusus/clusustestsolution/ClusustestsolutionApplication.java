bcf0c2f7f580c032384228a23fa8ec01dad4aa0b
pandeyraman <ramanpandey19@gmail.com>
2022-09-01 20:53:05 +0545

    initial setup

---
+++ src/main/java/com/clusus/clusustestsolution/ClusustestsolutionApplication.java
@@ -0,0 +1,13 @@
+package com.clusus.clusustestsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusustestsolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClusustestsolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clusus/clusustestsolution/ClusustestsolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clusus.clusustestsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusustestsolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
